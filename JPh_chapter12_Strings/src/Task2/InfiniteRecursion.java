5a8d281e506030d0f93ce497bcb41628d830b2c4
krivonos-alex <krivonos-al@mail.ru>
2018-11-20 15:13:49 +0300

    First test commit with Task2

---
+++ JPh_chapter12_Strings/src/Task2/InfiniteRecursion.java
@@ -0,0 +1,19 @@
+package Task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteRecursion {
+    @Override
+    public String toString() {
+        return "InfiniteRecursion adress: " + super.toString() /*this*/ + "\n";
+    }
+
+    public static void main(String[] args) {
+        List<InfiniteRecursion> recursionList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            recursionList.add(new InfiniteRecursion());
+        }
+        System.out.println(recursionList);
+    }
+}
